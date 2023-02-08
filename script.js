2a0d068e3361fee4710c5eb1ae097e02926cce34
Bobby Aston <bobby.j.aston@gmail.com>
2023-02-08 12:06:03 +0900

    implemented 5 day weather api call

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const ApiTestCall = async (lat, long, units) => {
+const currentWeatherCall = async (lat, long, units) => {
   const response = await fetch(
     "https://api.openweathermap.org/data/2.5/weather?lat=" +
       lat +
@@ -10,5 +10,20 @@ const ApiTestCall = async (lat, long, units) => {
     { mode: "cors" }
   );
   const weatherData = await response.json();
-  console.log(weatherData);
+  return weatherData;
+};
+
+const fiveDayForecastCall = async (lat, long, units) => {
+  const response = await fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+      lat +
+      "&lon=" +
+      long +
+      "&units=" +
+      units +
+      "&appid=7b3683382a3334888acbf0b14d740eb8",
+    { mode: "cors" }
+  );
+  const weatherData = await response.json();
+  return weatherData;
 };
