5476c8acba47b81a5c2ff1b2ce8e76e7118a4391
Bobby Aston <bobby.j.aston@gmail.com>
2023-03-07 17:33:49 +0900

    cleaned up and added styling + links

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const cwTopIcon = document.getElementById("current-weather-image-top");
 const cwTopTemp = document.getElementById("current-weather-temp-top");
-const cwTopDesc = document.getElementById("current-weather-decription-top");
+const cwTopDesc = document.getElementById("current-weather-description-top");
 const cwTopMaxTemp = document.getElementById("current-weather-max-temp-top");
 const cwTopMinTemp = document.getElementById("current-weather-min-temp-top");
 const cwTopWindSpeed = document.getElementById(
@@ -12,10 +12,12 @@ const cwTopWindDirection = document.getElementById(
 const cwTopVisibility = document.getElementById(
   "current-weather-visibility-top"
 );
+const cwLiftTopData = document.getElementById("current-weather-lift-top-data");
+const cwTopSnow1h = document.getElementById("current-weather-snow1hr-top");
 
 const cwBaseIcon = document.getElementById("current-weather-image-base");
 const cwBaseTemp = document.getElementById("current-weather-temp-base");
-const cwBaseDesc = document.getElementById("current-weather-decription-base");
+const cwBaseDesc = document.getElementById("current-weather-description-base");
 const cwBaseMaxTemp = document.getElementById("current-weather-max-temp-base");
 const cwBaseMinTemp = document.getElementById("current-weather-min-temp-base");
 const cwBaseWindSpeed = document.getElementById(
@@ -27,10 +29,54 @@ const cwBaseWindDirection = document.getElementById(
 const cwBaseVisibility = document.getElementById(
   "current-weather-visibility-base"
 );
+const cwLiftBaseData = document.getElementById("current-weather-base-data");
+const cwBaseSnow1h = document.getElementById("current-weather-snow1hr-base");
 
-const call = () => {
+// buttons
+
+const akakurakankoButton = document.getElementById("akakurakanko-button");
+const akakuraonsenButton = document.getElementById("akakuraonsen-button");
+const suginoharaButton = document.getElementById("suginohara-button");
+const ikenotairaButton = document.getElementById("ikenotaira-button");
+const araiButton = document.getElementById("arai-button");
+const kurohimeButton = document.getElementById("kurohime-button");
+const sekiButton = document.getElementById("seki-button");
+
+const kankoCall = () => {
   currentWeatherCall(36.886861, 138.177443, 36.893108, 138.140884, "metric");
 };
+
+const onsenCall = () => {
+  currentWeatherCall(36.890995, 138.176503, 36.896176, 138.156155, "metric");
+};
+
+const sekiCall = () => {
+  currentWeatherCall(36.906055, 138.160725, 36.906575, 138.152843, "metric");
+};
+
+const ikenotairaCall = () => {
+  currentWeatherCall(36.872381, 138.17161, 36.878033, 138.140635, "metric");
+};
+
+const suginoharaCall = () => {
+  currentWeatherCall(36.85659, 138.154716, 36.876923, 138.115566, "metric");
+};
+
+const kurohimeCall = () => {
+  currentWeatherCall(36.826836, 138.161452, 36.824649, 138.146912, "metric");
+};
+
+const araiCall = () => {
+  currentWeatherCall(36.990898, 138.180891, 36.993112, 138.138351, "metric");
+};
+
+akakurakankoButton.addEventListener("click", () => kankoCall());
+akakuraonsenButton.addEventListener("click", () => onsenCall());
+sekiButton.addEventListener("click", () => sekiCall());
+ikenotairaButton.addEventListener("click", () => ikenotairaCall());
+suginoharaButton.addEventListener("click", () => suginoharaCall());
+kurohimeButton.addEventListener("click", () => kurohimeCall());
+araiButton.addEventListener("click", () => araiCall());
 
 const currentWeatherCall = async (
   baseLat,
@@ -56,20 +102,20 @@ const currentWeatherCall = async (
       "http://openweathermap.org/img/wn/" +
       baseWeatherData.weather[0].icon +
       ".png";
-    cwBaseTemp.innerHTML = toString(
-      Math.round(baseWeatherData.main.temp) + "<sup>°C</sup>"
-    );
+    cwBaseTemp.innerHTML =
+      Math.round(baseWeatherData.main.temp) + "<sup>°C</sup>";
     cwBaseDesc.textContent = baseWeatherData.weather[0].description;
-    cwBaseMaxTemp.textContent = toString(
-      Math.round(baseWeatherData.main.temp_max) + " °C"
-    );
-    cwBaseMinTemp.textContent = toString(
-      Math.round(baseWeatherData.main.temp_min) + " °C"
-    );
-    cwBaseWindSpeed.textContent = toString(baseWeatherData.wind.speed) + " m/s";
-    cwBaseWindDirection.textContent = toString(baseWeatherData.wind.deg) + " °";
-    cwBaseWindDirection.textContent =
-      toString(baseWeatherData.visibility) + " m";
+    cwBaseMaxTemp.textContent =
+      Math.round(baseWeatherData.main.temp_max) + " °C";
+    cwBaseMinTemp.textContent =
+      Math.round(baseWeatherData.main.temp_min) + " °C";
+    cwBaseWindSpeed.textContent = baseWeatherData.wind.speed + " m/s";
+    cwBaseWindDirection.textContent = baseWeatherData.wind.deg + " °";
+    cwBaseWindDirection.textContent = baseWeatherData.visibility + " m";
+    if (baseWeatherData.snow["1h"] != undefined) {
+      cwBaseSnow1h.textContent =
+        "Last hour snowfall: " + baseWeatherData.snow["1h"];
+    }
   })();
 
   (async () => {
@@ -89,19 +135,18 @@ const currentWeatherCall = async (
       "http://openweathermap.org/img/wn/" +
       topWeatherData.weather[0].icon +
       ".png";
-    cwTopTemp.innerHTML = toString(
-      Math.round(topWeatherData.main.temp) + "<sup>°C</sup>"
-    );
+    cwTopTemp.innerHTML =
+      Math.round(topWeatherData.main.temp) + "<sup>°C</sup>";
     cwTopDesc.textContent = topWeatherData.weather[0].description;
-    cwTopMaxTemp.textContent = toString(
-      Math.round(topWeatherData.main.temp_max) + " °C"
-    );
-    cwTopMinTemp.textContent = toString(
-      Math.round(topWeatherData.main.temp_min) + " °C"
-    );
-    cwTopWindSpeed.textContent = toString(topWeatherData.wind.speed) + " m/s";
-    cwTopWindDirection.textContent = toString(topWeatherData.wind.deg) + " °";
-    cwTopWindDirection.textContent = toString(topWeatherData.visibility) + " m";
+    cwTopMaxTemp.textContent = Math.round(topWeatherData.main.temp_max) + " °C";
+    cwTopMinTemp.textContent = Math.round(topWeatherData.main.temp_min) + " °C";
+    cwTopWindSpeed.textContent = topWeatherData.wind.speed + " m/s";
+    cwTopWindDirection.textContent = topWeatherData.wind.deg + " °";
+    cwTopWindDirection.textContent = topWeatherData.visibility + " m";
+    if (topWeatherData.snow["1h"] != undefined) {
+      cwTopSnow1h.textContent =
+        "Last hour snowfall: " + topWeatherData.snow["1h"];
+    }
   })();
 };
 
@@ -118,3 +163,5 @@ const fiveDayForecastCall = async (lat, long, units) => {
   );
   const weatherData = await response.json();
 };
+
+kankoCall();
